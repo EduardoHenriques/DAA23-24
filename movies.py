81178a0b31be61619ed2e2149a1c5f87b382272c
EduardoHenriques <fernando28henriques@gmail.com>
2023-11-21 16:10:22 +0000

    MLP working, plots charted and dataset filters improved

--- movies.py
+++ movies.py
@@ -1,7 +1,7 @@
 # must install openpyxl in conda envoirnment to write the resulsts to an excel file
 from scipy import stats
 import sklearn as skl
-import os, datetime
+import os, datetime, warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,6 +9,7 @@ import seaborn as sns
 from datetime import date
 import category_encoders as ce  # Must install category_encoders in conda envoirnment
 from sklearn import preprocessing
+from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import GridSearchCV, KFold, train_test_split, cross_val_score
 # Model Imports
@@ -23,68 +24,105 @@ from scikeras.wrappers import KerasRegressor          # |
 #Metrics Imports
 from sklearn.metrics import make_scorer, mean_absolute_error, mean_squared_error, r2_score
 
+pd.set_option('mode.chained_assignment', None)
+
 # Dataset Path
 DATAFILE_PATH = "dataset/movies.csv"
 
 # remove useless columns, and apply data processing
 def filter(dataset):
-    print("###############BEFORE DROP###################")
-    print(dataset.info())
-    dataset.drop(columns=['id','tagline','backdrop_path','homepage','overview','production_companies','production_countries','poster_path'],axis=1,inplace=True)
+    print('-'*60 + "\nORIGINAL DATESET\n" + '-'*60)
+    # DROP USELESS COLUMNS/INFO, REMOVE MISSING VALUES
+    dataset.info()
+    dataset.drop(columns=['id','name','created_by','original_name','tagline','backdrop_path','homepage','overview','production_companies','production_countries','poster_path'],axis=1,inplace=True)
     dataset.dropna(subset=['first_air_date',
                           'last_air_date',
-                          'genres','created_by',
+                          'genres',
                           'languages','networks','origin_country',
                           'spoken_languages'],inplace=True)
-    dataset = dataset[(dataset['number_of_seasons']>0) & (dataset['number_of_seasons'] < 7)]
-    print("###############AFTER DROP###################")
-    print(dataset.info())
-    # turn first_air_date and last_air_date into days_aired
+    # DAYS_AIRED ADDED, REMOVED DATES
     dataset['first_air_date'] = pd.to_datetime(dataset['first_air_date'], format = '%Y-%m-%d', errors='coerce')
     dataset['last_air_date'] = pd.to_datetime(dataset['last_air_date'], format = '%Y-%m-%d', errors='coerce')
-    today = datetime.datetime.today()
     dataset['days_aired'] = (dataset['last_air_date'] - dataset['first_air_date']).dt.days
     dataset['days_aired'] = dataset['days_aired'][dataset['days_aired'] >= 0]
-    dataset.drop(columns=['first_air_date','last_air_date'])
-    # adult goes from 0/1 rather than False/True
+    dataset['days_aired'] = dataset['days_aired'].fillna(1)
+    dataset.drop(columns=['last_air_date','first_air_date'],axis=1,inplace=True)
+    # ADULT AS BOOL
     dataset['adult'] = dataset['adult'].astype(int)
-    # label encoding to a few attributes
+    dataset['in_production'] = dataset['in_production'].astype(int)
+    # LABEL ENCODING
     lb_make = LabelEncoder()
-    data['status'] = lb_make.fit_transform(data['status'])
-    print("############### STATUS ###################")
-    print(data['status'])
-    data['type'] = lb_make.fit_transform(data['type'])
-    data['spoken_languages'] = lb_make.fit_transform(data['spoken_languages'])
-    data['genres'] = lb_make.fit_transform(data['genres'])
-    data['origin_country'] = lb_make.fit_transform(data['origin_country'])
-    data['languages'] = lb_make.fit_transform(data['languages'])
-    data['networks'] = lb_make.fit_transform(data['networks'])
-    # remove outliers
+    columns_to_encode = ['status', 'type', 'spoken_languages', 'genres', 
+                     'origin_country', 'languages', 'networks', 
+                     'original_language']
+    dataset[columns_to_encode] = dataset[columns_to_encode].apply(lambda col: lb_make.fit_transform(col))
+    # REMOVE OUTLIERS
     # not sure about how good this technique is, but ~3000 rows were removed.
     # Z-score(number of sdt deviations from the mean) must be >3 to remove a row.
-    data.info()
-    print("AFTER")
-    numeric_columns = data.select_dtypes(include=['number'])
-    z_scores = np.abs(stats.zscore(numeric_columns))
-    outlier_rows = (z_scores > 3).any(axis=1)
-    dataset_no_outliers = data[~outlier_rows]
-    dataset_no_outliers.info()
+    numeric_columns = dataset.select_dtypes(include=['number']).columns
+    z_scores = np.abs(stats.zscore(dataset[numeric_columns]))
+    outlier_rows = (z_scores > 10).any(axis=1)
+    dataset_no_outliers = dataset[~outlier_rows]
+    print('-'*60 + "\nFILTERED DATASET\n" + '-'*60)
+    dataset_no_outliers.info() 
+    #dataset_no_outliers = dataset_no_outliers[(dataset_no_outliers['number_of_seasons']>0) & (dataset_no_outliers['number_of_seasons'] <= 7)]
+    dataset_no_outliers['number_of_seasons'] = np.where(dataset_no_outliers['number_of_seasons'] >= 6, '6', dataset_no_outliers['number_of_seasons'])
+    #dataset_no_outliers['number_of_seasons'] = pd.to_numeric(dataset_no_outliers['number_of_seasons'], errors='coerce').astype('Int64')
+    # REMOVE PILOT EPISODES
+    dataset_no_outliers = dataset_no_outliers[dataset_no_outliers['days_aired'] > 0]
+    # RESULTS
+    dataset_no_outliers.info() 
     return dataset_no_outliers
     
 # correlação, heat map, historgramas, estatísticas, etc... """
 def analysis(dataset):
     dataset.describe()
-    # dataset_nums = dataset.select_dtypes(include=['number'])
-    fig = plt.figure(figsize=(10,10))
-    #correlation = dataset_nums.corr(method = 'pearson')
-    #sns.stripplot(x='number_of_seasons', y='days_aired', data=dataset, jitter=True, alpha=0.7)
-    #sns.violinplot(x='number_of_seasons', y='days_aired', data=dataset)
-    incidents_count = dataset['number_of_seasons'].value_counts()
+    #
+    fig = plt.figure(figsize=(13, 13))
+    correlation = dataset.corr(method = 'pearson')
+    sns.heatmap(correlation, annot=True, cmap='coolwarm', fmt=".2f")
+    plt.savefig('corr.png')
+    plt.show()
+    #
+    fig = plt.figure(figsize=(10, 10))
+    sns.stripplot(x='number_of_seasons', y='days_aired', data=dataset, jitter=True, alpha=0.7)
+    plt.xlabel('Nº Seasons')
+    plt.ylabel('Days Aired')
+    plt.savefig('seasons_vs_days.png')
+    plt.show()
+    #
+    fig = plt.figure(figsize=(10, 10))
+    incidents_count = dataset['number_of_seasons'].value_counts().sort_index()
     sns.set(style="darkgrid")
     sns.barplot(x=incidents_count.index, y=incidents_count.values)
     plt.title("Season Nº frequency")
     plt.xlabel("Number", fontsize=12)
     plt.ylabel("Ammount", fontsize=12)
+    plt.savefig('season_amm.png')
+    plt.show()
+    #
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(dataset['popularity'], dataset['vote_average'])
+    plt.xlabel('Popularity')
+    plt.ylabel('Vote Average')
+    plt.title('Scatter Plot: Popularity vs Vote average')
+    plt.savefig('popularit_vs_voteAVG.png')
+    plt.show()
+    #
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(dataset['days_aired'], dataset['vote_average'])
+    plt.xlabel('Days Aired')
+    plt.ylabel('Vote Average')
+    plt.title('Scatter Plot: Days Aired vs Vote average')
+    plt.savefig('days_vs_voteAVG.png')
+    plt.show()
+    #
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(dataset['number_of_episodes'], dataset['days_aired'])
+    plt.xlabel('Episodes')
+    plt.ylabel('Days Aired')
+    plt.title('Scatter Plot: Episodes vs Days Aired')
+    plt.savefig('episodes_vs_days.png')
     plt.show()
     
 def int_scorer(trueVAL,predVAL):
@@ -94,16 +132,6 @@ def int_scorer(trueVAL,predVAL):
 
 def modelo1_LinearReg(dataset):
     lb_make = LabelEncoder()
-    dataset['status'] = lb_make.fit_transform(data['status'])
-    dataset['type'] = lb_make.fit_transform(data['type'])
-    dataset['spoken_languages'] = lb_make.fit_transform(data['spoken_languages'])
-    dataset['genres'] = lb_make.fit_transform(data['genres'])
-    dataset['origin_country'] = lb_make.fit_transform(data['origin_country'])
-    dataset['original_language'] = lb_make.fit_transform(data['original_language'])
-    dataset['languages'] = lb_make.fit_transform(data['languages'])
-    dataset['networks'] = lb_make.fit_transform(data['networks'])
-    dataset['in_production'] = dataset['in_production'].astype(int)
-    dataset.drop(columns=['name','original_name','created_by'],axis=1,inplace=True)
     print("\n\n\nLINEAR REGRESSION\n\n\n")
     dataset.info()
     ###
@@ -112,7 +140,6 @@ def modelo1_LinearReg(dataset):
     excel_PATH = 'resultados/resultados_linearReg.xlsx'
     dataset.info()
     # >>>> MODEL
-    dataset = dataset.select_dtypes(include=['number'])
     X = dataset.drop(columns=[parameter])
     Y = dataset[parameter]
     normalizer = MinMaxScaler()
@@ -134,12 +161,10 @@ def modelo1_LinearReg(dataset):
     save_result(result, excel_PATH)
     
 def modelo2_DecisionTree(dataset):
-    no_folds = 300
+    no_folds = 10
     parameter = 'number_of_seasons'
     excel_PATH = 'resultados/resultados_tree.xlsx'
     # <<<<< MODEL
-    dataset = dataset.drop(columns=['name','original_language','original_name','created_by','last_air_date','first_air_date'])
-    dataset.info()
     X = dataset.drop(columns=[parameter])
     Y = dataset[parameter]
     clf = DecisionTreeClassifier(random_state=2023)
@@ -157,8 +182,7 @@ def modelo2_DecisionTree(dataset):
 
 def modelo3_RandomForest(dataset):
     no_folds = 10
-    estimators = 500
-    dataset = dataset.select_dtypes(include=['number'])
+    estimators = 100
     parameter = 'number_of_seasons'
     excel_PATH = 'resultados/resultados_forest.xlsx'
     # <<<<< MODEL
@@ -184,29 +208,36 @@ def modelo4_MLP(dataset):
     excel_PATH = 'resultados/resultados_MLP.xlsx'
     print("###############BEFORE SELECT NUMB###################")
     print(dataset.info())
-    dataset = dataset.select_dtypes(include=['number'])
     parameter = 'number_of_seasons'
+    activation = 'relu'
+    rate = 0.01
     print("PERCEPTRON MODEL SETUP")
-    my_model = build_model()
+    my_model = build_model(activation,rate)
     # hyperparameter tuning
     optimizer = ['SGD','RMSprop','Adagrad']  
     param_grid = dict(optimizer=optimizer)
     kf = KFold(n_splits=5, shuffle=True,random_state=2021)
     model = KerasRegressor(model = my_model, batch_size=32, validation_split=0.2, epochs=20)
-    #my_model.summary()
-    # split data
     y = dataset[parameter]
-    X = dataset.drop(columns=parameter)
-    print("###############AFTER SELECT NUMB###################")
-    print(X.info())
-    X.fillna(0, inplace=True)
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=101)
-    #grid_search = GridSearchCV(estimator=model, param_grid =param_grid, cv = kf, scoring='neg_mean_absolute_error', refit='True', verbose=10)
+    x = dataset.drop(columns=parameter)
+    
+    normalizer = MinMaxScaler()
+    X_scaled = normalizer.fit_transform(x)
+    Y_scaled = normalizer.fit_transform(y.values.reshape(-1, 1))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled,Y_scaled,test_size=0.2,random_state=101)
     model.fit(X_train,y_train)
-    #print("Best %f  using %s is:"  % (grid_search.best_score_,grid_search.best_params_))
-    means = model.cv_results_['mean_test_score']
-    stds = model.cv_results_['std_test_score']
-    params = model.cv_results_['params']
+    predictions = model.predict(X_test)
+    print(f"MAE: {metrics.mean_absolute_error(y_test, predictions)}\n MSE: {metrics.mean_squared_error(y_test, predictions)}")
+    result = {
+        'Parâmetro': [parameter],
+        'Activation': [activation],
+        'Rate': [rate],
+        'MSE': [round(metrics.mean_squared_error(y_test, predictions),2)],
+        'MAE': [round(metrics.mean_absolute_error(y_test, predictions),2)]
+    }
+    save_result(result, excel_PATH)
+    
     
 def save_result(result, file_path):
     if not os.path.exists(file_path):
@@ -219,9 +250,12 @@ def save_result(result, file_path):
     print(f"Model result saved in:{file_path}")
 
 # builds a basic MLP model
-def build_model(activation='relu', learning_rate=0.01):
+def build_model(activation, learning_rate):
     model = Sequential() 
-    model.add(Dense(8, input_dim = 16, activation = activation))
+    model.add(Dense(32, input_dim = 16, activation = activation))
+    model.add(Dense(16, activation = activation))
+    model.add(Dense(8, activation = activation))
+    model.add(Dense(4, activation = activation))
     model.add(Dense(1, activation = activation))
     model.compile(
         loss = 'mae',
@@ -232,16 +266,15 @@ def build_model(activation='relu', learning_rate=0.01):
 
 # START
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore", category=FutureWarning, module="seaborn")
     if os.path.exists(DATAFILE_PATH):
         print("Reading Data...")
         data = pd.read_csv(DATAFILE_PATH)
         data = filter(data)
-        print("###############AFTER FILTER##########################")
-        print(data.info())
         analysis(data)
-        print("\n\n\nAFTER FILTERING...\n\n\n")
         #modelo1_LinearReg(data)
-        modelo4_MLP(data)
+        #modelo4_MLP(data)
+        #modelo2_DecisionTree(data)
         #modelo3_RandomForest(data)
     else:
         print(f"Error: File '{DATAFILE_PATH}' does not exist.")
